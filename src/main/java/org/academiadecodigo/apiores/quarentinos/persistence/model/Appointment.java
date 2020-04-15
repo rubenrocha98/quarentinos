23165b5d951efcfbf22ab07698d4d03ef9c37e11
Francisco Carreiro <psychop60@gmail.com>
2020-04-15 16:58:23 +0000

    provider, appointment && roleTypes implementation DONE

---
+++ src/main/java/org/academiadecodigo/apiores/quarentinos/persistence/model/Appointment.java
@@ -0,0 +1,43 @@
+package org.academiadecodigo.apiores.quarentinos.persistence.model;
+
+import java.util.Date;
+
+public class Appointment extends AbstractModel {
+
+    private int id;
+    private Date date;
+    private Client client;
+    private Provider provider;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Provider getProvider() {
+        return provider;
+    }
+
+    public void setProvider(Provider provider) {
+        this.provider = provider;
+    }
+}
